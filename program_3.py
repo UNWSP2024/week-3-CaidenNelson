daa3aff64b784c82171ad639f1e4cf025f91cba6
CaidenNelson <cwnelson4@students.unwsp.edu>
2024-09-20 17:33:50 -0500

    Update program_3.py

--- program_3.py
+++ program_3.py
@@ -8,18 +8,30 @@
 # Over 6 pounds but not more than 10 pounds	$4.00
 # Over 10 pounds	$4.75
 # Write a program which calculates the shipping charge and displays the total.
-
+#Proggramer = Caiden Nelson
+#Date = 9/19/24
+#Freight Shipping Calculator
 def weight_conversion(weight):
     # Calculate the shipping charge.
-    shippingCost = 0.0
+    if weight <= 2:
+        shippingCost = 1.50
+    elif weight >2 and weight <6:
+        shippingCost = 3.00
+    elif weight >6 and weight <10:
+        shippingCost = 4.00
+    else:
+        shippingCost = 4.75
+
+
     ######################
     # WRITE YOUR CODE HERE
     ######################
-    
+
     return shippingCost
 
+
 #### This piece of the code has been done for you,
-#### you only need to worry about the actual shipping 
+#### you only need to worry about the actual shipping
 #### charge logic in the weight_conversion function
 if __name__ == '__main__':
     # Local variables
@@ -29,4 +41,4 @@ if __name__ == '__main__':
     weight = float(input('Enter the weight of the package: '))
     # Display the shipping charge.
     shippingCost = weight_conversion(weight)
-    print ('Shipping charge: $', format(shippingCost, '.2f'))
+    print('Shipping charge: $', format(shippingCost, '.2f'))
